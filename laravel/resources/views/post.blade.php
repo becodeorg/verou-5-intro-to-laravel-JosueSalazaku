dcd634376ea4ea47da1965b98ed4a3ce6ffb1e8b
Josué Salazaku <josuesalazaku@gmail.com>
2024-01-22 12:29:59 +0100

    Add a go back link to posts

--- laravel/resources/views/post.blade.php
+++ laravel/resources/views/post.blade.php
@@ -9,6 +9,6 @@
 <a href="/post"><h1>Fan art 1 PAGE</h1></a>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, velit eaque. Laudantium deleniti vitae temporibus mollitia necessitatibus quibusdam cum, excepturi aliquam vero sint. Ad ea nobis assumenda molestiae magni perferendis!</p>
     <br>
-    <a href="/"> GO BACK </a>
+    <a href="/t"> GO BACK </a>
 </body>
 </html>

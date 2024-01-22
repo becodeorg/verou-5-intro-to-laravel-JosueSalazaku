fae8bafd65af5b7a139025b6474d5f328e3a493c
Josué Salazaku <josuesalazaku@gmail.com>
2024-01-22 12:21:31 +0100

    Add h1 links ass named routes with text

---
+++ laravel/resources/views/posts.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="/post"><h1>Fan art 1 PAGE</h1></a>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, velit eaque. Laudantium deleniti vitae temporibus mollitia necessitatibus quibusdam cum, excepturi aliquam vero sint. Ad ea nobis assumenda molestiae magni perferendis!</p>
+    <br>
+    <a href="/post"><h1>Fan art 2 PAGE</h1></a>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo unde, ad debitis libero sint quo veniam possimus doloremque ducimus a ab ex. Ad reprehenderit a tenetur obcaecati quam deleniti nemo!</p>
+    <br>
+    <a href="/post"><h1>Fan art 3 PAGE</h1></a>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae hic voluptas iste sapiente, perspiciatis ducimus. Nulla dolore hic esse recusandae. Est ipsum eius aperiam reprehenderit error quasi quia dolores facilis.</p>
+    <br>
+    <a href="/post"><h1>Fan art 4 PAGE</h1></a>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel architecto unde deleniti veritatis tenetur quaerat quis necessitatibus aut, laborum ad fugiat, perferendis, molestias ipsa expedita id deserunt optio ipsum corrupti!</p>
+</body>
+</html>

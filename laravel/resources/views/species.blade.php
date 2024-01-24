4ae8622ee8aba139db574dc1cde133b849c98081
Josué Salazaku <josuesalazaku@gmail.com>
2024-01-24 16:02:05 +0100

    Add a list of species in the species page

--- laravel/resources/views/species.blade.php
+++ laravel/resources/views/species.blade.php
@@ -1,15 +1,22 @@
+<?php require '/Applications/XAMPP/xamppfiles/htdocs/php/verou-5-intro-to-laravel-JosueSalazaku/laravel/routes/aliens.php'; ?>
+
 @vite('resources/css/app.css')
-    <main class="h-screen bg-indigo-400 flex flex-col">
-    <header class="p-8 bg-slate-900 text-white text-xl ">
-        <ul class="flex flex-row justify-around ">
-        <li class="font-bold"><a href="{{"posts"}}">Xenopedia</a></li>
-        <li><a href="{{"species"}}">Species</a></li>
-        <li>games</li>
-    </ul></header>
+<main class="h-screen bg-indigo-400 flex flex-col">
+    <header class="p-8 bg-slate-900 text-white text-xl">
+        <ul class="flex flex-row justify-around">
+            <li class="font-bold"><a href="{{"posts"}}">Xenopedia</a></li>
+            <li><a href="{{"species"}}">Species</a></li>
+            <li>games</li>
+        </ul>
+    </header>
 
-<article class=" mt-8 mx-20 mb-20">
+    <?php foreach ($alienSpecies as $alien): ?>
+        <article class="mt-8 mx-20 mb-20">
+            <h2><?= $alien['name']; ?></h2>
+            <p>Type: <?= $alien['type']; ?></p>
+            <p>Solar System: <?= $alien['solar_system']; ?></p>
+        </article>
+    <?php endforeach; ?>
 
-</article>
-    <footer class="bg-sky-800 text-white flex flex-row justify-center ">Welcome to our universe</footer>
-    </main>
-
+    <footer class="bg-sky-800 text-white flex flex-row justify-center">Welcome to our universe</footer>
+</main>
---
+++ laravel/routes/aliens.php
@@ -0,0 +1,62 @@
+<?php
+$alienSpecies = [
+    [
+        'name' => 'The Visitors (V)',
+        'type' => 'unknown',
+        'solar_system' => 'Alpha Centauri',
+    ],
+
+    [
+        'name' => 'The Monolith (2001: A Space Odyssey)',
+        'type' => 'unknown',
+        'solar_system' => 'Zeta Reticuli',
+    ],
+
+    [
+        'name' => 'Vulcans (Star Trek)',
+        'type' => 'humanoid',
+        'solar_system' => 'Vulcan System',
+    ],
+
+    [
+        'name' => 'Twi\'leks (Star Wars)',
+        'type' => 'humanoid',
+        'solar_system' => 'Ryloth System',
+    ],
+
+    [
+        'name' => 'Predators (Predator)',
+        'type' => 'bipedal',
+        'solar_system' => 'Yautja Prime',
+    ],
+
+    [
+        'name' => 'Ewoks (Star Wars)',
+        'type' => 'bipedal',
+        'solar_system' => 'Endor System',
+    ],
+
+    [
+        'name' => 'Tauntauns (Star Wars)',
+        'type' => 'quadrupedal',
+        'solar_system' => 'Hoth System',
+    ],
+
+    [
+        'name' => 'Hexapods (Arrival)',
+        'type' => 'quadrupedal',
+        'solar_system' => 'Cancri System',
+    ],
+
+    [
+        'name' => 'Silurians (Doctor Who)',
+        'type' => 'lizard',
+        'solar_system' => 'Earth',
+    ],
+
+    [
+        'name' => 'Yautja (Predator)',
+        'type' => 'lizard',
+        'solar_system' => 'Yautja Prime',
+    ],
+];
